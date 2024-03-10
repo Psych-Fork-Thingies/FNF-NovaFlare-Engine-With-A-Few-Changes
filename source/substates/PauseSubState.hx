7440cbb3472a96a8108f13bee43a587a35159184
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2024-03-10 08:49:44 -0300

    Update PauseSubState.hx

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -664,7 +664,6 @@ class PauseSubState extends MusicBeatSubstate
         			PlayState.deathCounter = 0;
         			PlayState.seenCutscene = false;
     
-        			Mods.loadTopMod();
         			if(PlayState.isStoryMode) {
         				MusicBeatState.switchState(new StoryMenuState());
         			} else {
